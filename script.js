0dc9c5e1a5790b01d34d58daa9ddf50d1bab3936
Charlie Haynes <37176278+CharlieHaynes@users.noreply.github.com>
2023-12-28 10:41:10 +0000

    Wrote playRound function.
    The function with if/elseif/else statements and checks the
    computerSelection and playerSelection values against each other and
     returns a string based on the result.

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ let choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    playerSelection.toLowerCase() === choices[0] &&
+    computerSelection === choices[1]
+  ) {
+    return "Computer wins this round!";
+  } else if (
+    playerSelection.toLowerCase() === choices[1] &&
+    computerSelection === choices[2]
+  ) {
+    return "Computer wins this round!";
+  } else if (
+    playerSelection.toLowerCase() === choices[2] &&
+    computerSelection === choices[0]
+  ) {
+    return "Computer wins this round!";
+  } else {
+    return "You win this round!";
+  }
+}
