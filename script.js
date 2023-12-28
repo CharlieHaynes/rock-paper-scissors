c2fdf22689d8bef06453075c075eca5a4999cda6
Charlie Haynes <37176278+CharlieHaynes@users.noreply.github.com>
2023-12-28 11:46:43 +0000

    Fixed bug and created game function.
    The bug was using && logical operators in getPlayerChoice functions
    conditional statement.
    game function loops playRound function 5 times.

--- script.js
+++ script.js
@@ -12,25 +12,22 @@ function getPlayerChoice() {
     playerChoice.toLowerCase() !== choices[2]
   ) {
     alert("You typed a choice wrong, please try again!");
-    getPlayerChoice();
+    return getPlayerChoice();
   } else {
-    return playerChoice;
+    return playerChoice.toLowerCase();
   }
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (
-    playerSelection.toLowerCase() === choices[0] &&
-    computerSelection === choices[1]
-  ) {
+  if (playerSelection === choices[0] && computerSelection === choices[1]) {
     return "Computer wins this round!";
   } else if (
-    playerSelection.toLowerCase() === choices[1] &&
+    playerSelection === choices[1] &&
     computerSelection === choices[2]
   ) {
     return "Computer wins this round!";
   } else if (
-    playerSelection.toLowerCase() === choices[2] &&
+    playerSelection === choices[2] &&
     computerSelection === choices[0]
   ) {
     return "Computer wins this round!";
@@ -41,8 +38,14 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-console.log("You chose: " + playerSelection);
-console.log("The computer chose: " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  for (let i = 0; i <= 4; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    console.log("You chose: " + playerSelection);
+    console.log("The computer chose: " + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+
+game();
