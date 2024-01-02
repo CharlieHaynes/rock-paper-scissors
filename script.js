7ae0bc0d251eb711488860bf41bda9c53bfa7dc1
Charlie Haynes <37176278+CharlieHaynes@users.noreply.github.com>
2024-01-02 21:51:09 +0000

    Updated to include a simple UI.
    Add 3 buttons and a div element to control and see output from.

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
-let choices = ["rock", "paper", "scissors"];
 let playerWin = 0;
 let computerWin = 0;
+let roundsPlayed = 0;
+const choices = ["rock", "paper", "scissors"];
+
+const rockBtn = document.querySelector("#rock-btn");
+const paperBtn = document.querySelector("#paper-btn");
+const scissorsBtn = document.querySelector("#scissors-btn");
+const result = document.querySelector("#result");
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-function getPlayerChoice() {
-  let playerChoice = prompt("Choose: rock, paper or scissors!");
-  if (
-    playerChoice.toLowerCase() !== choices[0] &&
-    playerChoice.toLowerCase() !== choices[1] &&
-    playerChoice.toLowerCase() !== choices[2]
-  ) {
-    alert("You typed a choice wrong, please try again!");
-    return getPlayerChoice();
-  } else {
-    return playerChoice.toLowerCase();
+rockBtn.addEventListener("click", () => {
+  playRound(choices[0], getComputerChoice());
+});
+paperBtn.addEventListener("click", () => {
+  playRound(choices[1], getComputerChoice());
+});
+scissorsBtn.addEventListener("click", () => {
+  playRound(choices[2], getComputerChoice());
+});
+
+function displayResult() {
+  if (playerWin > computerWin && roundsPlayed >= 5) {
+    result.textContent = `You win! With a final score of ${playerWin} to ${computerWin}.`;
+  } else if (computerWin > playerWin && roundsPlayed >= 5) {
+    result.textContent = `Computer wins! With a final score of ${computerWin} to ${playerWin}.`;
+  } else if (roundsPlayed >= 5) {
+    result.textContent = `Draw! With a final score of ${playerWin} to ${computerWin}.`;
   }
 }
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === choices[0] && computerSelection === choices[1]) {
-    return "Computer wins this round!";
+    computerWin++;
+    roundsPlayed++;
+    result.textContent = `Computer wins this round! ${playerWin} - ${computerWin}`;
   } else if (
     playerSelection === choices[1] &&
     computerSelection === choices[2]
   ) {
-    return "Computer wins this round!";
+    computerWin++;
+    roundsPlayed++;
+    result.textContent = `Computer wins this round! ${playerWin} - ${computerWin}`;
   } else if (
     playerSelection === choices[2] &&
     computerSelection === choices[0]
   ) {
-    return "Computer wins this round!";
+    computerWin++;
+    roundsPlayed++;
+    result.textContent = `Computer wins this round! ${playerWin} - ${computerWin}`;
   } else if (playerSelection === computerSelection) {
-    return "This round is a draw!";
+    roundsPlayed++;
+    result.textContent = `This round is a draw! ${playerWin} - ${computerWin}`;
   } else {
-    return "You win this round!";
+    playerWin++;
+    roundsPlayed++;
+    result.textContent = `You win this round! ${playerWin} - ${computerWin}`;
   }
-}
-
-function game() {
-  for (let i = 0; i <= 4; i++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    console.log("You chose: " + playerSelection);
-    console.log("The computer chose: " + computerSelection);
-    let roundWinner = playRound(playerSelection, computerSelection);
-    console.log(roundWinner);
-    console.log(" ");
-    if (roundWinner === "You win this round!") {
-      playerWin++;
-    } else if (roundWinner === "Computer wins this round!") {
-      computerWin++;
-    }
+  if (roundsPlayed >= 5) {
+    displayResult();
+    roundsPlayed = 0;
+    playerWin = 0;
+    computerWin = 0;
   }
-}
-
-game();
-
-if (playerWin > computerWin) {
-  console.log(`You win! With a score of ${playerWin} to ${computerWin}.`);
-} else if (computerWin > playerWin) {
-  console.log(`Computer won! With a score of ${computerWin} to ${playerWin}.`);
-} else {
-  console.log(`Draw! With a score of ${playerWin} to ${computerWin}.`);
+  console.log(`Computer selected: ${computerSelection}`);
 }
